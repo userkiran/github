cb892e356aab86c4fb27226e959370d267c9ffea
userkiran <88781561+userkiran@users.noreply.github.com>
2021-08-12 12:37:54 +0530

    Update km.tf

--- km.tf
+++ km.tf
@@ -3,15 +3,6 @@ region = "ap-south-1"
 access_key = "AKIAS6I325TB7CWEU34J"
 secret_key = "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"
 }
-resource "aws_instance" "ec2-instance" {
-ami = "ami-0443305dabd4be2bc"
-instance_type = "t2.micro"
-key_name = "userkiran"
-count = "2"
-tags = {
-        Name = "Linux_Jenkins"
-}
-}
 resource "aws_s3_bucket" "terrabucket" {
 bucket = "bucket"
 acl = "private"
