d4cc543bfcbcaa68f61def25b8440a9322726cd1
userkiran <88781561+userkiran@users.noreply.github.com>
2021-08-12 14:01:14 +0530

    Update km.tf
    rds

--- km.tf
+++ km.tf
@@ -4,8 +4,15 @@ access_key = "AKIAS6I325TBTQPRVSV2"
 secret_key = "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"
 }
 
-resource "aws_s3_bucket" "terrabucket" {
-bucket = "hellokirankumar325"
-acl = "private"
+resource "aws_db_instance" "terrformrds" {
+  allocated_storage    = 10
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t3.micro"
+  name                 = "mydb"
+  username             = "kiran"
+  password             = "terraform"
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = true
 }
 
