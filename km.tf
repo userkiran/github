dc3bd6ab64ac9a772fa09f5ed4f35457235bcd05
userkiran <88781561+userkiran@users.noreply.github.com>
2021-08-12 13:37:55 +0530

    Update km.tf

--- km.tf
+++ km.tf
@@ -1,9 +1,29 @@
 provider "aws" {
 region = "ap-south-1"
-access_key = "AKIAS6I325TB6VUPMGES"
-secret_key = "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"
+access_key = "AKIAS6I325TBTQPRVSV2"
+secret_key = "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"
+}
+resource "aws_instance" "trlinux" {
+ami = "ami-0443305dabd4be2bc"
+instance_type = "t2.micro"
+key_name = "userkiran"
+count = "2"
+tags = {
+        Name = "terraform"
+}
 }
 resource "aws_s3_bucket" "terrabucket" {
 bucket = "hellokirankumar1234"
 acl = "private"
+}
+resource "aws_db_instance" "terrformrds" {
+  allocated_storage    = 10
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t3.micro"
+  name                 = "mydb"
+  username             = "kiran"
+  password             = "terraform"
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = true
 }
