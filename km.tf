ebf9f44f65c16d6260c2933def433b9c72e20db0
userkiran <88781561+userkiran@users.noreply.github.com>
2021-08-12 14:08:25 +0530

    Update km.tf

--- km.tf
+++ km.tf
@@ -4,13 +4,13 @@ access_key = "AKIAS6I325TBTQPRVSV2"
 secret_key = "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"
 }
 
-resource "aws_db_instance" "terrformrds" {
-  allocated_storage    = 10
+resource "aws_db_instance" "terds" {
+  allocated_storage    = 20
   engine               = "mysql"
   engine_version       = "5.7"
   instance_class       = "db.t3.micro"
   name                 = "mydb"
-  username             = "kiran"
+  username             = "kirankumar"
   password             = "terraform"
   parameter_group_name = "default.mysql5.7"
   skip_final_snapshot  = true
