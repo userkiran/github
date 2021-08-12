d9d83fa2d9754ac56f0bab41e86e7c09a4c9f8b5
userkiran <88781561+userkiran@users.noreply.github.com>
2021-08-12 13:53:56 +0530

    Update km.tf

--- km.tf
+++ km.tf
@@ -3,15 +3,7 @@ region = "ap-south-1"
 access_key = "AKIAS6I325TBTQPRVSV2"
 secret_key = "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"
 }
-resource "aws_instance" "trlinux" {
-ami = "ami-0443305dabd4be2bc"
-instance_type = "t2.micro"
-key_name = "userkiran"
-count = "2"
-tags = {
-        Name = "terraform"
-}
-}
+
 resource "aws_s3_bucket" "terrabucket" {
 bucket = "hellokirankumar325"
 acl = "private"
