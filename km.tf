c9e5084a69078bcf8da512300da7bd0d0441bd9c
userkiran <88781561+userkiran@users.noreply.github.com>
2021-08-12 13:28:21 +0530

    Update km.tf

--- km.tf
+++ km.tf
@@ -1,7 +1,7 @@
 provider "aws" {
 region = "ap-south-1"
-access_key = "AKIAS6I325TB7CWEU34J"
-secret_key = "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"
+access_key = "AKIAS6I325TB6VUPMGES"
+secret_key = "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"
 }
 resource "aws_s3_bucket" "terrabucket" {
 bucket = "hellokirankumar1234"
