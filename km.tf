35a7901191a99b55b9d91d30b771a36014d439d9
userkiran <88781561+userkiran@users.noreply.github.com>
2021-08-12 12:49:55 +0530

    Update km.tf

--- km.tf
+++ km.tf
@@ -4,6 +4,6 @@ access_key = "AKIAS6I325TB7CWEU34J"
 secret_key = "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"
 }
 resource "aws_s3_bucket" "terrabucket" {
-bucket = "bucketkiranjenkin"
+bucket = "secondbucketkiranjenkin"
 acl = "private"
 }
